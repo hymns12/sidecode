ebacb42783416f673bbaceba1bfb443d4845d0a7
hymns12 <nickcloudsyinka1@gmail.com>
2024-02-08 07:38:08 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {ethers}  from 'ethers';
+import WalletConnet from './WalletConnet';
+
+function App() {
+  return (
+    <div className="App">
+      <WalletConnet />
+    </div>
+  );
+}
+
+export default App;
